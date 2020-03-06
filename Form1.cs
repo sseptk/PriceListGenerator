0843acf29ddbdc26401b3f246f085c5c022b369b
sseptk <progvdm@gmail.com>
2020-03-06 17:36:19 +0200

    Added display data in a table interface

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,28 @@ namespace PriceListGenerator
         public PriceList()
         {
             InitializeComponent();
+
+            fillingTheGridWithData();
+        }
+
+        private void fillingTheGridWithData()
+        {
+            JsonParser jsonParser = new JsonParser();
+
+            var Products = jsonParser.DeserializeJsonObejct();
+
+            int numberRow = 0;
+            foreach (var v in Products)
+            {
+                DataGrid_products.Rows.Add();
+                DataGrid_products.Rows[numberRow].Cells["id"].Value = v.id;
+                DataGrid_products.Rows[numberRow].Cells["title"].Value = v.title;
+                DataGrid_products.Rows[numberRow].Cells["price"].Value = v.price;
+                DataGrid_products.Rows[numberRow].Cells["currency"].Value = v.currency;
+                DataGrid_products.Rows[numberRow].Cells["description"].Value = v.description;
+                DataGrid_products.Rows[numberRow].Cells["rating"].Value = v.rating;
+                ++numberRow;
+            }
         }
     }
 }
